9ce324f913bdcd3d0d7e6ee814bb7c63d8d361d5
Kachkynov Alikhan <t8t228@gmail.com>
2024-09-15 17:44:56 +0600

    не хочу больше комитить(

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import React, { useState } from 'react';
 import './App.css';
 import Ball from "./components/Ball/Ball.tsx";
 
-const App = () => {
-  return (
-    <>
-      <Ball/>
-    </>
-  )
+const generateRandomNumber = ():number[] => {
+    const numbers:number[] = [];
+
+    while(numbers.length < 5){
+        const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+
+        if(!numbers.includes(randomNumber)){
+            numbers.push(randomNumber);
+        }
+    }
+
+    return numbers.sort((a,b) => a-b);
+}
+
+const App: React.FC = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const newNumbers = () => {
+        setNumbers(generateRandomNumber());
+    }
+
+
+    return (
+        <>
+            <div className="App">
+                <button type="button" onClick={newNumbers}>New Number</button>
+
+                <div className={'balls-container'}>
+                    {numbers.map(number => (
+                        <Ball number={number} key={number} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
-export default App
+export default App;
--- src/components/Ball/Ball.tsx
+++ src/components/Ball/Ball.tsx
@@ -1,11 +1,16 @@
+import React from "react";
 import './Ball.css';
 
-const Ball = () => {
-    return (
-        <div>
+interface BallProps {
+    number: number;
+}
 
+const Ball: React.FC<BallProps> = ({number}) => {
+    return(
+        <div className="ball">
+            {number}
         </div>
-    );
-};
+    )
+}
 
 export default Ball;
